7b3a3c8de49f8cd435634b96edfd6eb246b6b0a4
Jackmc <jackmc1122@gmail.com>
2019-09-03 15:20:40 -0700

    Add routes, model and tests for making and getting animals

--- test/app.test.js
+++ test/app.test.js
@@ -8,13 +8,42 @@ const mongoose = require('mongoose');
 describe('app routes', () => {
   beforeAll(() => {
     connect();
-  });
-
-  beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('creates an animal', () => {
+    return request(app)
+      .post('/api/v1/animals')
+      .send({ name: 'fred', type: 'Lama', diet: 'herbavor', weight: '60lbs', age: 5 })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          _id: expect.any(String),
+          name: 'fred', 
+          type: 'Lama', 
+          diet: 'herbavor', 
+          weight: '60lbs', 
+          age: 5 
+        });
+      });
+  });
+
+  it('returns a list of animals', () => {
+    return request(app)
+      .get('/api/v1/animals')
+      .then(res => {
+        expect(res.body).toHaveLength(1);
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          name: 'fred', 
+          type: 'Lama', 
+          diet: 'herbavor', 
+          weight: '60lbs', 
+          age: 5 
+        });
+      });
   });
 });
